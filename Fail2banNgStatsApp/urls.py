c54ce7187b5405ecef1a2819f12a3c40444d83f8
Spooore <erward.erward@gmail.com>
2017-11-30 00:56:50 +0100

    Data Example

--- Fail2banNgStatsApp/urls.py
+++ Fail2banNgStatsApp/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    url(r'^line_chart/json/$', views.line_chart_json,
+        name='line_chart_json'),
+    url(r'^$', views.line_chart,name='line_chart'),
 ]
--- Fail2banNgStatsApp/views.py
+++ Fail2banNgStatsApp/views.py
@@ -1,4 +1,37 @@
 from django.shortcuts import render
+from flask import Flask
+from flask import Markup
+from flask import Flask
+from flask import render_template
+from random import randint
+from django.views.generic import TemplateView
+from chartjs.views.lines import BaseLineChartView
+
 
 def index(request):
     return render(request, 'index.html')
+
+
+def chart():
+    labels = ["January","February","March","April","May","June","July","August"]
+    values = [10,9,8,7,6,4,7,8]
+    return render_template('chart.html', values=values, labels=labels)
+
+
+class LineChartJSONView(BaseLineChartView):
+    def get_labels(self):
+        return ["Niedziela", "Poniedziałek", "Wtorek", "Środa"]
+
+    def get_providers(self):
+        return ["Chiny", "Korea Północna", "Ukraina"]
+
+    def get_data(self):
+        return [[30, 12, 5, 20],
+                [9, 17, 12, 6],
+                [1, 2, 4, 3]]
+
+
+line_chart = TemplateView.as_view(template_name='line_chart.html')
+line_chart_json = LineChartJSONView.as_view()
+
+
