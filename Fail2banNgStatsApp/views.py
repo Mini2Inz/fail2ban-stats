3d94901c4e95edb1e3d0e2c9916b6c665f64152e
Spooore <erward.erward@gmail.com>
2017-12-10 18:00:43 +0100

    requirements

--- Fail2banNgStatsApp/views.py
+++ Fail2banNgStatsApp/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render
 from flask import Flask
 from flask import Markup
@@ -5,6 +6,7 @@ from flask import Flask
 from flask import render_template
 from random import randint
 from django.views.generic import TemplateView
+from django import http
 from chartjs.views.lines import BaseLineChartView
 
 
@@ -34,4 +36,31 @@ class LineChartJSONView(BaseLineChartView):
 line_chart = TemplateView.as_view(template_name='line_chart.html')
 line_chart_json = LineChartJSONView.as_view()
 
+
+
+class ComplexEncoder(json.JSONEncoder):
+    def default(self, obj):
+        try:
+            return super(ComplexEncoder, obj).default(obj)
+        except TypeError:
+            if hasattr(obj, 'pk'):
+                return obj.pk
+            return str(obj)
+
+
+class JSONResponseMixin(object):
+    def render_to_response(self, context):
+        return self.get_json_response(self.convert_context_to_json(context))
+
+    def get_json_response(self, content, **httpresponse_kwargs):
+        return http.HttpResponse(content,
+                                 content_type='application/json',
+                                 **httpresponse_kwargs)
+
+    def convert_context_to_json(self, context):
+        return json.dumps(context, cls=ComplexEncoder)
+
+
+class JSONView(JSONResponseMixin, TemplateView):
+    pass
 
