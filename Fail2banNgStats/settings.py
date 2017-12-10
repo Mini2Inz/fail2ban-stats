79801b96e4b1c987ee4295decc75a365bd847413
Spooore <erward.erward@gmail.com>
2017-12-11 00:25:37 +0100

    charts and database models added

--- Fail2banNgStats/settings.py
+++ Fail2banNgStats/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'django_static_jquery',
     'Fail2banNgStatsApp',
-    'chartjs',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
--- Fail2banNgStatsApp/models.py
+++ Fail2banNgStatsApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class LineChartData(models.Model):
+    country = models.CharField(max_length=50)
+    dayOfWeek = models.CharField(max_length=50)
+    count = models.IntegerField()
+
+    class Meta:
+        db_table = "linechartdata"
