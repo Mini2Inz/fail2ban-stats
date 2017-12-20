d1d29db89a41183d987cec3191fc8671b839bda2
Spooore <erward.erward@gmail.com>
2017-12-20 01:35:29 +0100

    BansData and ChartData models added

--- Fail2banNgStatsApp/models.py
+++ Fail2banNgStatsApp/models.py
@@ -3,9 +3,19 @@ from django.utils import timezone
 
 
 class LineChartData(models.Model):
+    code = models.CharField(max_length=10,  default='UNKNOWN')
     country = models.CharField(max_length=50)
     dayOfWeek = models.CharField(max_length=50)
     count = models.IntegerField()
 
     class Meta:
         db_table = "linechartdata"
+
+class BansTableData(models.Model):
+    jail = models.CharField(max_length=50)
+    ip = models.CharField(max_length=50)
+    timeofban = models.IntegerField()
+    bantime = models.IntegerField()
+
+    class Meta:
+        db_table = "banstabledata"
