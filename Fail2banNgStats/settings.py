51c996c76963bcef31ce393d252a1d38957d4848
Spooore <erward.erward@gmail.com>
2018-01-04 17:36:16 +0100

    Celery init setup

--- Fail2banNgStats/settings.py
+++ Fail2banNgStats/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 
 import os
 
+# Celery settings
+
+CELERY_BROKER_URL = 'amqp://guest:guest@localhost//'
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
