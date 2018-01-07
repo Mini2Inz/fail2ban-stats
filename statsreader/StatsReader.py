b94060d59ea70adf57dd2c927d375da6e15cdc80
Marcin Ciborowski <ciborowskim@outlook.com>
2018-01-07 14:05:01 +0100

    initial StatsReader

---
+++ statsreader/StatsReader.py
@@ -0,0 +1,16 @@
+import csv
+import asyncio
+
+class StatsReader():
+    def __init__(self, config):
+        self.getFail2banHosts()        
+
+    def getBans(self):
+        pass
+
+    def getLocations(self):
+        pass
+
+    def getFail2banHosts(self):
+        pass
+    
---
+++ statsreader/main.py
@@ -0,0 +1,21 @@
+from statsreader import StatsReader
+from configparser import ConfigParser
+from argparse import ArgumentParser
+import schedule
+
+def setup_args():
+    pass
+
+def read_config():
+    pass
+
+def setup_scheduled_refresh():
+    pass
+
+def main():
+    setup_args()
+    read_config()
+    setup_scheduled_refresh()
+
+if __name__ == "__main__":
+    main()
